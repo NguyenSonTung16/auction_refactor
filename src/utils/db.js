8e39b262a97cd063c0e059e8757a9ed8429efff1
tinphan247 <tinphan111005@gmail.com>
2026-03-27 20:00:15 +0700

    Refactor account and seller routes, add OTP & Recaptcha services (DRY & SRP)

---
+++ src/utils/db.js
@@ -0,0 +1,12 @@
+import knex from 'knex';
+export default knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    post: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  pool: { min: 0, max: 7 }
+});
