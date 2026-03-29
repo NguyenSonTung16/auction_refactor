93da6ecbda3cf3ecedace6beb329d31da3378815
23120398 <vothanhminhtuea@gmail.com>
2026-03-29 11:29:58 +0700

    move logic from /admin/routes to controllers/admin

--- auction-web/src/utils/db.js
+++ auction-web/src/utils/db.js
@@ -3,7 +3,7 @@ export default knex({
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
-    post: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
